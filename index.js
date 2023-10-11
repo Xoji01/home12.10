d705a5323ad42c58afacecf4ee35e5e0b86c66bc
Xoji01 <135627056+Xoji01@users.noreply.github.com>
2023-10-11 19:44:38 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+let pwd =+prompt('password')
+let col = 5
+let par = 7777
+do {
+    if(pwd !==par)
+    {pwd = +prompt(`не правильно у вас ещё ${col} попытки`)}
+    else if (swd==par, col==5){
+        alert('ok')}
+       col--
+    
+} while (col>0);
